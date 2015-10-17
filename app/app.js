de6ae94d58793aec7a37ea0bb62b30c8c1c51f7b
Tali Walt <taliwalt@gmail.com>
2015-10-16 17:34:05 -0700

    Added a method to the main controller, setCurrentCategory, so that the user can filter by category in the view.  Added a category filter to the bookmarks div in index.html

--- app/app.js
+++ app/app.js
@@ -1,21 +1,34 @@
-angular.module('Taggly', [])
-	.controller('MainCtrl', function ($scope) {
-		            $scope.categories = [
-			            {"id": 0, "name": "Development"},
-			            {"id": 1, "name": "Design"},
-			            {"id": 2, "name": "Exercise"},
-			            {"id": 3, "name": "Humor"}
-		            ];
+angular.module ( 'Taggly', [] )
+    .controller ( 'MainCtrl', function( $scope ) {
+    $scope.categories = [
+        { "id": 0, "name": "Development" },
+        { "id": 1, "name": "Design" },
+        { "id": 2, "name": "Exercise" },
+        { "id": 3, "name": "Humor" }
+    ];
 
-		            $scope.bookmarks = [
-			            {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
-			            {"id": 1, "title": "Egghead.io", "url": "http://egghead.io", "category": "Development" },
-			            {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
-			            {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
-			            {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
-			            {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-			            {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-			            {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-			            {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
-		            ]
-	            })
+    $scope.bookmarks       = [
+        { "id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+        { "id": 1, "title": "Egghead.io", "url": "http://egghead.io", "category": "Development" },
+        { "id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+        { "id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+        { "id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+        { "id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+        { "id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+        { "id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+        { "id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+    ]
+
+    $scope.currentCategory = null
+
+    function isCurrentCategory ( category ){
+        return $scope.CurrentCategory !== null && category.name === $scope.currentCategory.name
+    }
+    function setCurrentCategory ( category ){
+        $scope.currentCategory = category
+    }
+
+    $scope.isCurrentCategory = isCurrentCategory
+    $scope.setCurrentCategory = setCurrentCategory
+
+} )
